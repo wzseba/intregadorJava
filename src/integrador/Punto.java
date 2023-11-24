bbcb4bad238f75745ab3a229e428b0e2143b7b42
wzseba <wzseba@gmail.com>
2023-11-23 23:06:21 -0300

    add: clase de apoyo para la resolucion de la clase cuadrante

---
+++ src/integrador/Punto.java
@@ -0,0 +1,20 @@
+package integrador;
+
+public class Punto {
+
+	private double x;
+	private double y;
+	
+	public Punto(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
+}
