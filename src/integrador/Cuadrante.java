56e51d08881794df88b5294d993266019a6f4c3f
wzseba <wzseba@gmail.com>
2023-11-23 23:05:14 -0300

    add: nueva clase en proceso de resolucion

---
+++ src/integrador/Cuadrante.java
@@ -0,0 +1,14 @@
+package integrador;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Cuadrante {
+
+	public List<Punto> readCuadrante(String archivo) {
+
+		List<Punto> puntoAux = new LinkedList<Punto>();
+
+		return puntoAux;
+	}
+}
